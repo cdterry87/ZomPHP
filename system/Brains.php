2ae904d412deb8087bc76a56f12c749f0d099fc5
= <chase.terry87@gmail.com>
2017-06-26 21:52:10 -0500

    Added some basic helper functions

---
+++ system/Brains.php
@@ -0,0 +1,26 @@
+<?php
+
+/* ----------------------------------------------------------------------
+ * Redirect to the specified page.
+ * ---------------------------------------------------------------------- */
+function redirect($page){
+	extract($GLOBALS);
+	
+	header('location: '.$config['base_url'].$page, true);
+	exit;
+}
+
+/* ----------------------------------------------------------------------
+ * Get the URI.  If a segment is specified, only get that segment.
+ * ---------------------------------------------------------------------- */
+function get_uri($segment=''){
+	$uri=$_SERVER['REQUEST_URI'];
+	
+	if(trim($segment)!='' and is_numeric($segment)){
+		$segments=explode('/', $uri);
+		if(array_key_exists($segment, $segments)){
+			return $segments[$segment];
+		}
+	}
+	return $uri;
+}
